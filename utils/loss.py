89ad94d586875298193fb9c7eb67652a7fc55ff2
Juan Aguirre <jucaguirrear@unal.edu.co>
2021-06-24 18:29:37 -0500

    changed tf function by tf.py_function

--- utils/loss.py
+++ utils/loss.py
@@ -2,14 +2,13 @@ import tensorflow as tf
 from scipy.ndimage import distance_transform_edt as eucl_distance
 
 
-@tf.function
 def distMaps(seg):
     posmask = seg > 0.5
     negmask = ~ posmask
     return eucl_distance(negmask)* negmask - (eucl_distance(posmask) - 1) * posmask
 
 def boundary_loss(y_true,y_pred):
-    distmap = distMaps(y_true)
+    distmap = tf.py_function(func= distMaps, inp=[y_true], Tout=tf.float32)
     value = (dist_map*x_pred - dist_map*x)
     return tf.reduce_sum(value)
 
