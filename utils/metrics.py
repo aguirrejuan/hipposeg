d7194a637e91f7522326e35dd5483a203b45a0df
Juan Carlos Aguirre Arango <jucaguirrear@unal.edu.co>
2021-06-28 19:11:57 -0500

    corrected metric Dice, added **2

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,5 +1,7 @@
 import tensorflow as tf 
 
+#metrics per vol
+
 def jaccard(y_true,y_pred):
     inter = tf.reduce_sum(y_true*y_pred)
     union = tf.reduce_sum(y_true) + tf.reduce_sum(y_pred) - inter
@@ -7,7 +9,7 @@ def jaccard(y_true,y_pred):
 
 def dice(y_true,y_pred):
     nue = 2*tf.reduce_sum(y_true*y_pred)
-    den = tf.reduce_sum(y_true) + tf.reduce_sum(y_pred)
+    den = tf.reduce_sum(y_true**2) + tf.reduce_sum(y_pred**2)
     return nue/den
 
 def precision(y_true,y_pred):
