0f0ab2faf3d71d2afe67299cc383bf4390859e6a
Juan Carlos Aguirre Arango <jucaguirrear@unal.edu.co>
2021-06-30 17:05:15 -0500

    Update plot.py

--- utils/plot.py
+++ utils/plot.py
@@ -7,10 +7,12 @@ def plot_predict(x,y,y_pred):
     (None,None,3),(None,None,1)(bool),(None,None,1)(bool)
     """
     plt.figure(figsize=(6,6))
+    x = tf.reduce_mean(x,axis=-1)[...,np.newaxis]
     mask  = x * (1 - tf.cast(tf.math.logical_or(y,y_pred),dtype=tf.float32))
     Rojo  = mask + tf.cast(tf.math.logical_and(~y,y_pred),dtype=tf.float32)  #false positive
     Verde = mask + tf.cast(tf.math.logical_and(y,y_pred),dtype=tf.float32)  #true positive
     Azul  = mask + tf.cast(tf.math.logical_and(y,~y_pred),dtype=tf.float32)  #false negative
+    print(tf.shape(x))
     image =  np.concatenate((Rojo,Verde,Azul),axis=2)
     plt.imshow(image)
     plt.show()
